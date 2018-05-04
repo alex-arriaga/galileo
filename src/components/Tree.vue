f1b7d1166e05b45025e58d7a52a4a2795adb6387
Vicente Grajeda <vicente.grajeda@gmail.com>
2018-05-04 16:10:45 -0400

    Add Tree component placeholder

---
+++ src/components/Tree.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="b-tree">I am a tree</div>
+</template>
+
+<script>
+  export default {
+    name: 'Tree',
+    data () {
+      return {
+        levels: 3
+      }
+    }
+  }
+</script>
